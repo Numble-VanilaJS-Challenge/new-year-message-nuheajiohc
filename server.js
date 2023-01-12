ebff8794b65565af6ac2a76d598009cf73dc6e92
nuheajiohc <jaehun341012@gmail.com>
2023-01-12 20:09:54 +0900

    feat: 서버 구축

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// 모든 페이지에서 서버 돌아가게 하는 코드
+app.use("/", express.static(path.resolve(__dirname, "./")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./", "index.html"));
+});
+
+app.listen(8080, () => console.log("Server running"));
