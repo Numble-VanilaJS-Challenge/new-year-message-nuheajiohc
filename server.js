b97d00027f2410cf9b332651fec991fb513c91ad
nuheajiohc <jaehun341012@gmail.com>
2023-01-18 23:46:23 +0900

    refactor: 서버파일 수정

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
+const port = 9500;
 
-// 모든 페이지에서 서버 돌아가게 하는 코드
-app.use("/", express.static(path.resolve(__dirname, "./")));
+app.use("/", express.static(__dirname));
+app.get("/*", (req, res) => res.sendFile(__dirname + "/index.html"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./", "index.html"));
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
-
-app.listen(8080, () => console.log("Server running"));
